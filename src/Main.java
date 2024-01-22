73459e670b48708fb7ae024010a29ec28f18efbe
Bharath-Raj-07 <bharathrajlifeline@gmail.com>
2024-01-22 16:16:02 +0530

    Added: Calculate Salary including Part time

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,31 @@
 public class Main{
     public static void main(String[] args) {
         Employee e1 = new Employee("Bharath",107,200);
-        int check = (int)((Math.random()*10))%2;
+        int check = (int)((Math.random()*10))%3;
 
-        if(check == 0){
-            e1.attendance = false;
-        }
-        else {
-            e1.attendance = true;
+        if (check == 0){
+            e1.attendance = 0;
+        }else if (check == 1) {
+            e1.attendance = 1;
+        }else {
+            e1.attendance = 2;
         }
 
-        if(e1.attendance){
+        if(e1.attendance==1){
             System.out.println("Employee is present");
             e1.workHour = 8;
-        }else {
+        }else if(e1.attendance == 0) {
             System.out.println("Employee is absent");
+        }else {
+            System.out.println("Employee is part time");
+            e1.workHour = 4;
         }
+
         //it is a Not static method
         Main m1 = new Main();
         System.out.println(m1.calculateSalary(e1));
+
+
     }
     double calculateSalary(Employee e){
         return e.workHour*e.wagePerHour;
